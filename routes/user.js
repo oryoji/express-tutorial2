63f61decf8cec2b14b843adc112c22e83767a54f
oryoji <naoto.oryoji@gmail.com>
2023-02-18 23:07:24 +0900

    ルーティングの基礎を学習しました

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('ユーザーです');
+});
+
+router.get('/info', (req, res) => {
+    res.send('ユーザー情報です');
+});
+
+router.get('/:id', (req, res) => {
+    res.send(`${req.params.id}のユーザー情報を取得しました`);
+});
+
+module.exports = router;
